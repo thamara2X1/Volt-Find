14d93ab8ea83f2bf6606b0aa61894e1ce7cc82e7
thamara2X1 <vininduvtph@gmail.com>
2025-11-09 20:29:57 +0530

    Initial project setup with Firebase and main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:volt_find/OnboardingScreen.dart';
 import 'package:volt_find/LoginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:volt_find/SignupScreen.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -22,7 +30,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const OnboardingScreen(),
         '/login': (context) => const LoginScreen(),
-        // '/signup': (context) => const SignUpScreen(),
+        '/signup': (context) => const SignUpScreen(),
       },
       
 
